14887b8a585fcf1d22bccfd7021be54f8644af1a
ETdoFresh <ETdoFresh@gmail.com>
2018-12-19 12:15:47 -0600

    Convert Synchronous Server to Asynchronous Server.

--- WebSocketBridge/Program.cs
+++ WebSocketBridge/Program.cs
@@ -8,71 +8,129 @@ using System.Text;
 
 namespace WebSocketBridge
 {
+    // TODO : Handle errors in sending and receiving
+    // TODO : Handle disconnects
+
     class Program
     {
         private const string EOL = "\r\n";
-        private static TcpListener server;
-        private static TcpClient client;
-        private static NetworkStream stream;
 
         private static IPAddress iPAddress = IPAddress.Parse("192.168.254.194");
         private static int port = 9080;
 
-        static void Main(string[] args)
+        public static List<TcpClient> clients = new List<TcpClient>();
+
+        private static void Main(string[] args)
         {
             StartServer();
-
-            client = server.AcceptTcpClient();
-            Console.WriteLine("A client connected! " + client.Client.RemoteEndPoint);
-            stream = client.GetStream();
-
-            HandleWebSocketHandshake();
-            SendWebSocketClientInstructions();
-
-            while (true)
-            {
-                while (!stream.DataAvailable || !client.Connected) ;
-                if (!client.Connected) break;
-                byte[] bytes = ReceiveWebSocketBytes();
-                byte[] decoded = DecodeWebSocketBytes(bytes);
-                var message = Encoding.UTF8.GetString(decoded);
-                Console.WriteLine("From WebSocketClient: " + message);
-
-                Socket socket = ConnectTcpSocket();
-                if (socket.Connected)
-                {
-                    SentToTcpClient(message, socket);
-                    bytes = ReceiveTcpClientBytes(socket);
-                    var output = Encoding.UTF8.GetString(bytes);
-                    Console.WriteLine("From TcpSocket: " + output);
-                    SendToWebSocket(output);
-                    socket.Close();
-                }
-                else
-                    SendToWebSocket("Could not connect to socket!");
-            }
+            while (true) ;
         }
 
-        private static byte[] ReceiveTcpClientBytes(Socket socket)
+        private static void StartServer()
+        {
+            var server = new TcpListener(IPAddress.Parse("127.0.0.1"), 9081);
+            server.Start();
+            BeginAcceptConnection(server);
+        }
+
+        private static void BeginAcceptConnection(TcpListener listener)
+        {
+            Console.WriteLine("Server: Waiting for a connection...");
+            listener.BeginAcceptTcpClient(OnAcceptedConnection, listener);
+        }
+
+        private static void OnAcceptedConnection(IAsyncResult ar)
+        {
+            TcpListener listener = (TcpListener)ar.AsyncState;
+            TcpClient client = listener.EndAcceptTcpClient(ar);
+            clients.Add(client);
+            var clientIndex = clients.Count - 1;
+            Console.WriteLine("Client " + clientIndex + ": Connected from " + client.Client.RemoteEndPoint);
+            BeginReceiveHandshake(client);
+            BeginAcceptConnection(listener);
+        }
+
+        private static void BeginReceiveHandshake(TcpClient client)
         {
             var buffer = new byte[64 * 1024];
-            var bytesRead = socket.Receive(buffer);
-            var bytes = new byte[bytesRead];
-            Array.Copy(buffer, 0, bytes, 0, bytesRead);
-            return bytes;
+            var stream = client.GetStream();
+            stream.BeginRead(buffer, 0, buffer.Length, OnReceivedHandshake, new ClientBuffer(client, stream, buffer));
         }
 
-        private static void SentToTcpClient(string message, Socket socket)
+        private static void OnReceivedHandshake(IAsyncResult ar)
         {
+            var clientBuffer = (ClientBuffer)ar.AsyncState;
+            var client = clientBuffer.client;
+            var stream = clientBuffer.stream;
+            var clientIndex = clients.IndexOf(client);
+            var bytesRead = stream.EndRead(ar);
+            var message = Encoding.UTF8.GetString(clientBuffer.bytes, 0, bytesRead);
+            Console.WriteLine("Client " + clientIndex + ": Received **Handshake**");
+
+            BeginSendHandshake(client, stream, message);
+        }
+
+        private static void BeginSendHandshake(TcpClient client, NetworkStream stream, string message)
+        {
+            var key = GetKey(message);
+            var response = Encoding.UTF8.GetBytes(
+                           "HTTP/1.1 101 Switching Protocols" + EOL
+                           + "Connection: upgrade" + EOL
+                           + "Upgrade: websocket" + EOL
+                           + "Sec-WebSocket-Accept: " + key + EOL
+                           + EOL);
+            stream.BeginWrite(response, 0, response.Length, OnSentHandshake, new ClientBuffer(client, stream, response));
+        }
+
+        private static void OnSentHandshake(IAsyncResult ar)
+        {
+            var clientBuffer = (ClientBuffer)ar.AsyncState;
+            var client = clientBuffer.client;
+            var stream = clientBuffer.stream;
+            var clientIndex = clients.IndexOf(client);
+
+            stream.EndWrite(ar);
+            Console.WriteLine("Client " + clientIndex + ": Sent **Handshake**");
+
+            SendWebSocketClientInstructions(stream);
+            Console.WriteLine("Client " + clientIndex + ": Sent **Instructions**");
+
+            BeginReceiveMessage(client, stream);
+        }
+
+        private static void BeginReceiveMessage(TcpClient client, NetworkStream stream)
+        {
+            var buffer = new byte[64 * 1024];
+            stream.BeginRead(buffer, 0, buffer.Length, OnReceivedMessage, new ClientBuffer(client, stream, buffer));
+        }
+
+        private static void OnReceivedMessage(IAsyncResult ar)
+        {
+            var clientBuffer = (ClientBuffer)ar.AsyncState;
+            var client = clientBuffer.client;
+            var stream = clientBuffer.stream;
+            var bytes = clientBuffer.bytes;
+            var clientIndex = clients.IndexOf(client);
+            var bytesRead = stream.EndRead(ar);
+
+            var trimmed = new byte[bytesRead];
+            Array.Copy(bytes, trimmed, trimmed.Length);
+            var decoded = DecodeWebSocketBytes(trimmed);
+            var message = Encoding.UTF8.GetString(decoded);
+            Console.WriteLine("Client " + clientIndex + ": Received " + message);
+
+            var socket = ConnectTcpSocket();
             socket.Send(Encoding.UTF8.GetBytes(message));
-            Console.WriteLine("To TcpSocket: " + message);
-        }
+            Console.WriteLine("Client " + clientIndex + ": tcp://" + iPAddress + ":" + port + ": Sent " + message);
+            bytes = ReceiveTcpClientBytes(socket);
+            socket.Close();
 
-        private static Socket ConnectTcpSocket()
-        {
-            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            socket.Connect(iPAddress, port);
-            return socket;
+            var response = Encoding.UTF8.GetString(bytes);
+            Console.WriteLine("Client " + clientIndex + ": tcp://" + iPAddress + ":" + port + ": Received " + response);
+
+            SendToWebSocket(stream, response);
+            Console.WriteLine("Client " + clientIndex + ": Sent " + response);
+            BeginReceiveMessage(client, stream);
         }
 
         private static byte[] DecodeWebSocketBytes(byte[] bytes)
@@ -88,39 +146,25 @@ namespace WebSocketBridge
             return decoded;
         }
 
-        private static byte[] ReceiveWebSocketBytes()
+        private static Socket ConnectTcpSocket()
         {
-            var bytes = new byte[client.Available];
-            stream.Read(bytes, 0, bytes.Length);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(iPAddress, port);
+            return socket;
+        }
+
+        private static void SendToTcpClient(string message, Socket socket)
+        {
+            
+        }
+
+        private static byte[] ReceiveTcpClientBytes(Socket socket)
+        {
+            var buffer = new byte[64 * 1024];
+            var bytesRead = socket.Receive(buffer);
+            var bytes = new byte[bytesRead];
+            Array.Copy(buffer, 0, bytes, 0, bytesRead);
             return bytes;
-        }
-
-        private static void HandleWebSocketHandshake()
-        {
-            var message = ReceiveHandshakeMessage();
-            var key = GetKey(message);
-            SendHandshakeResponse(key);
-        }
-
-        private static void SendWebSocketClientInstructions()
-        {
-            SendToWebSocket("Welcome to WebSocketBridge.");
-            SendToWebSocket("This will relay commands from this websocket to a tcp socket");
-            SendToWebSocket("Please send the server ip and port in the following format:");
-            SendToWebSocket("TcpSocket Server: " + iPAddress.ToString());
-            SendToWebSocket("Port: " + port);
-            SendToWebSocket("Note: The above are the default settings.");
-        }
-
-        private static void SendHandshakeResponse(string key)
-        {
-            var response = Encoding.UTF8.GetBytes(
-                            "HTTP/1.1 101 Switching Protocols" + EOL
-                            + "Connection: upgrade" + EOL
-                            + "Upgrade: websocket" + EOL
-                            + "Sec-WebSocket-Accept: " + key + EOL
-                            + EOL);
-            stream.Write(response, 0, response.Length);
         }
 
         private static string GetKey(string message)
@@ -134,30 +178,17 @@ namespace WebSocketBridge
             return key;
         }
 
-        private static void StartServer()
+        private static void SendWebSocketClientInstructions(NetworkStream stream)
         {
-            server = new TcpListener(IPAddress.Parse("127.0.0.1"), 9081);
-            server.Start();
-            Console.WriteLine("Server started... awaiting connection...");
+            SendToWebSocket(stream, "Welcome to WebSocketBridge.");
+            SendToWebSocket(stream, "This will relay commands from this websocket to a tcp socket");
+            SendToWebSocket(stream, "Please send the server ip and port in the following format:");
+            SendToWebSocket(stream, "IPAddress: " + iPAddress.ToString());
+            SendToWebSocket(stream, "Port: " + port);
+            SendToWebSocket(stream, "Note: The above are the default settings.");
         }
 
-        private static string ReceiveHandshakeMessage()
-        {
-            string message;
-            while (!stream.DataAvailable) ;
-            var bytes = new byte[client.Available];
-            stream.Read(bytes, 0, bytes.Length);
-            message = Encoding.UTF8.GetString(bytes);
-            Console.WriteLine("Read: " + message);
-            return message;
-        }
-
-        private static void OnAcceptTcpClient(IAsyncResult ar)
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void SendToWebSocket(string message)
+        private static void SendToWebSocket(NetworkStream stream, string message)
         {
             List<byte> bytes = new List<byte>();
             bytes.Add(129);
@@ -174,7 +205,27 @@ namespace WebSocketBridge
                 bytes.AddRange(BitConverter.GetBytes((ulong)message.Length).Reverse());
             }
             bytes.AddRange(Encoding.UTF8.GetBytes(message));
-            stream.Write(bytes.ToArray(), 0, bytes.Count);
+            stream.BeginWrite(bytes.ToArray(), 0, bytes.Count, OnSentToWebSocket, stream);
+        }
+
+        private static void OnSentToWebSocket(IAsyncResult ar)
+        {
+            var stream = (NetworkStream)ar.AsyncState;
+            stream.EndWrite(ar);
+        }
+
+        private class ClientBuffer
+        {
+            public TcpClient client;
+            public NetworkStream stream;
+            public byte[] bytes;
+
+            public ClientBuffer(TcpClient client, NetworkStream stream, byte[] bytes)
+            {
+                this.client = client;
+                this.stream = stream;
+                this.bytes = bytes;
+            }
         }
     }
 }
