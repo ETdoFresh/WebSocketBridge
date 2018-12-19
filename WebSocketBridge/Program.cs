5b81b1b295c9f36b35d7933bddc84e40764b9710
ETdoFresh <ETdoFresh@gmail.com>
2018-12-19 10:41:04 -0600

    Downgrade to .NET 3.5. Refactor Main() into seperatate functions.

--- WebSocketBridge/Program.cs
+++ WebSocketBridge/Program.cs
@@ -10,6 +10,7 @@ namespace WebSocketBridge
 {
     class Program
     {
+        private const string EOL = "\r\n";
         private static TcpListener server;
         private static TcpClient client;
         private static NetworkStream stream;
@@ -19,88 +20,144 @@ namespace WebSocketBridge
 
         static void Main(string[] args)
         {
-            server = new TcpListener(IPAddress.Parse("127.0.0.1"), 9081);
-            server.Start();
-            Console.WriteLine("Server started... awaiting connection...");
+            StartServer();
+
             client = server.AcceptTcpClient();
             Console.WriteLine("A client connected! " + client.Client.RemoteEndPoint);
             stream = client.GetStream();
 
-            while (!stream.DataAvailable) ;
-            var bytes = new byte[client.Available];
-            stream.Read(bytes, 0, bytes.Length);
-            for (int i = 0; i < bytes.Length; i++)
-                Console.Write((i == 0 ? "" : " ") + bytes[i].ToString());
-            Console.WriteLine();
-            var message = Encoding.UTF8.GetString(bytes);
-            Console.WriteLine("Read: " + message);
-
-            const string EOL = "\r\n";
-            var keyLabel = "Sec-WebSocket-Key: ";
-            var startIndex = message.IndexOf(keyLabel) + keyLabel.Length;
-            var endIndex = message.IndexOf(EOL, startIndex);
-            string key = message.Substring(startIndex, endIndex - startIndex);
-            key += "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
-            key = Convert.ToBase64String(SHA1.Create().ComputeHash(Encoding.UTF8.GetBytes(key)));
-            var response = Encoding.UTF8.GetBytes(
-                "HTTP/1.1 101 Switching Protocols" + EOL
-                + "Connection: upgrade" + EOL
-                + "Upgrade: websocket" + EOL
-                + "Sec-WebSocket-Accept: " + key + EOL
-                + EOL);
-            stream.Write(response, 0, response.Length);
-
-            Send("Welcome to WebSocketBridge.");
-            Send("This will relay commands from this websocket to a tcp socket");
-            Send("Please send the server ip and port in the following format:");
-            Send("TcpSocket Server: " + iPAddress.ToString());
-            Send("Port: " + port);
-            Send("Note: The above are the default settings.");
+            HandleWebSocketHandshake();
+            SendWebSocketClientInstructions();
 
             while (true)
             {
                 while (!stream.DataAvailable || !client.Connected) ;
                 if (!client.Connected) break;
-                bytes = new byte[client.Available];
-                stream.Read(bytes, 0, bytes.Length);
-                for (int i = 0; i < bytes.Length; i++)
-                    Console.Write((i == 0 ? "" : " ") + bytes[i].ToString());
-
-                var byte1 = bytes[0];
-                var byte2 = bytes[1];
-                var mask = new[] { bytes[2], bytes[3], bytes[4], bytes[5] };
-                var decoded = new byte[bytes.Length - 6];
-                Array.Copy(bytes, 6, decoded, 0, decoded.Length);
-
-                for (int i = 0; i < decoded.Length; i++)
-                    decoded[i] = (byte)(bytes[i + 6] ^ mask[i % 4]);
-                Console.WriteLine();
-
-                var clientDisconnected = decoded.Length == 2 && decoded[0] == 3 && decoded[1] == 233;
-                if (clientDisconnected)
-                    break;
-
-                message = Encoding.UTF8.GetString(decoded);
+                byte[] bytes = ReceiveWebSocketBytes();
+                byte[] decoded = DecodeWebSocketBytes(bytes);
+                var message = Encoding.UTF8.GetString(decoded);
                 Console.WriteLine("From WebSocketClient: " + message);
 
-                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-                socket.Connect(iPAddress, port);
+                Socket socket = ConnectTcpSocket();
                 if (socket.Connected)
                 {
-                    socket.Send(Encoding.UTF8.GetBytes(message));
-                    Console.WriteLine("To TcpSocket: " + message);
-                    byte[] buffer = new byte[64 * 1024];
-                    var bytesRead = socket.Receive(buffer);
-                    var output = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    SentToTcpClient(message, socket);
+                    bytes = ReceiveTcpClientBytes(socket);
+                    var output = Encoding.UTF8.GetString(bytes);
                     Console.WriteLine("From TcpSocket: " + output);
-                    Send(output);
+                    SendToWebSocket(output);
+                    socket.Close();
                 }
                 else
-                    Send("Could not connect to socket!");
+                    SendToWebSocket("Could not connect to socket!");
             }
         }
 
-        private static void Send(string message)
+        private static byte[] ReceiveTcpClientBytes(Socket socket)
+        {
+            var buffer = new byte[64 * 1024];
+            var bytesRead = socket.Receive(buffer);
+            var bytes = new byte[bytesRead];
+            Array.Copy(buffer, 0, bytes, 0, bytesRead);
+            return bytes;
+        }
+
+        private static void SentToTcpClient(string message, Socket socket)
+        {
+            socket.Send(Encoding.UTF8.GetBytes(message));
+            Console.WriteLine("To TcpSocket: " + message);
+        }
+
+        private static Socket ConnectTcpSocket()
+        {
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Connect(iPAddress, port);
+            return socket;
+        }
+
+        private static byte[] DecodeWebSocketBytes(byte[] bytes)
+        {
+            var byte1 = bytes[0];
+            var byte2 = bytes[1];
+            var mask = new[] { bytes[2], bytes[3], bytes[4], bytes[5] };
+            var decoded = new byte[bytes.Length - 6];
+            Array.Copy(bytes, 6, decoded, 0, decoded.Length);
+
+            for (int i = 0; i < decoded.Length; i++)
+                decoded[i] = (byte)(bytes[i + 6] ^ mask[i % 4]);
+            return decoded;
+        }
+
+        private static byte[] ReceiveWebSocketBytes()
+        {
+            var bytes = new byte[client.Available];
+            stream.Read(bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        private static void HandleWebSocketHandshake()
+        {
+            var message = ReceiveHandshakeMessage();
+            var key = GetKey(message);
+            SendHandshakeResponse(key);
+        }
+
+        private static void SendWebSocketClientInstructions()
+        {
+            SendToWebSocket("Welcome to WebSocketBridge.");
+            SendToWebSocket("This will relay commands from this websocket to a tcp socket");
+            SendToWebSocket("Please send the server ip and port in the following format:");
+            SendToWebSocket("TcpSocket Server: " + iPAddress.ToString());
+            SendToWebSocket("Port: " + port);
+            SendToWebSocket("Note: The above are the default settings.");
+        }
+
+        private static void SendHandshakeResponse(string key)
+        {
+            var response = Encoding.UTF8.GetBytes(
+                            "HTTP/1.1 101 Switching Protocols" + EOL
+                            + "Connection: upgrade" + EOL
+                            + "Upgrade: websocket" + EOL
+                            + "Sec-WebSocket-Accept: " + key + EOL
+                            + EOL);
+            stream.Write(response, 0, response.Length);
+        }
+
+        private static string GetKey(string message)
+        {
+            var keyLabel = "Sec-WebSocket-Key: ";
+            var startIndex = message.IndexOf(keyLabel) + keyLabel.Length;
+            var endIndex = message.IndexOf(EOL, startIndex);
+            var key = message.Substring(startIndex, endIndex - startIndex);
+            key += "258EAFA5-E914-47DA-95CA-C5AB0DC85B11";
+            key = Convert.ToBase64String(SHA1.Create().ComputeHash(Encoding.UTF8.GetBytes(key)));
+            return key;
+        }
+
+        private static void StartServer()
+        {
+            server = new TcpListener(IPAddress.Parse("127.0.0.1"), 9081);
+            server.Start();
+            Console.WriteLine("Server started... awaiting connection...");
+        }
+
+        private static string ReceiveHandshakeMessage()
+        {
+            string message;
+            while (!stream.DataAvailable) ;
+            var bytes = new byte[client.Available];
+            stream.Read(bytes, 0, bytes.Length);
+            message = Encoding.UTF8.GetString(bytes);
+            Console.WriteLine("Read: " + message);
+            return message;
+        }
+
+        private static void OnAcceptTcpClient(IAsyncResult ar)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void SendToWebSocket(string message)
         {
             List<byte> bytes = new List<byte>();
             bytes.Add(129);
